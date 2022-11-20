a6e3af90cfeea48daa35fc4f9df93bcc0da00b37
jeferson.medeiros <jeferson.medeiros@portotech.org>
2022-11-19 23:09:27 -0300

    upd to use constructor injection

--- src/main/java/com/senai/receiver/rabbitmq/RabbitReceiver.java
+++ src/main/java/com/senai/receiver/rabbitmq/RabbitReceiver.java
@@ -1,14 +1,16 @@
 package com.senai.receiver.rabbitmq;
 
 import com.senai.receiver.domain.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class RabbitReceiver {
 
     @RabbitListener(queues = RabbitConfig.queueName) // <-- FAZ A LIGAÇÃO COM UMA FILA
     public void consumer(Student student) {
-        System.out.println("Bem vindo ao Mini Curso sobre Microservicos e Comunicacao Assincrona: " + student.getName());
+        log.info("Bem vindo ao Mini Curso sobre Microservicos e Comunicacao Assincrona: " + student.getName());
     }
 }
